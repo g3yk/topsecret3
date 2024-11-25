b4957e71a3637c779e3e15cd725810e5edc30e13
g3yk <g3yk@users.noreply.github.com>
2024-11-25 03:47:56 +0300

    fix: resolve linting and test errors
    - Remove unnecessary whitespace characters from `config/routes.rb`.
    - Add `db/schema.db` file to prevent test errors related to database schema.
    - Implement new tests for all previously added pages to ensure comprehensive coverage.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,18 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    # get pages_home_url
+    get "/"
+    assert_response :success
+  end
+
+  test "should get about" do
+    get "/about"
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get "/contact"
     assert_response :success
   end
 end
