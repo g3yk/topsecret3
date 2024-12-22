1c7d4c7c2f47435e05d4bf90688bf6911476e2e9
MrLucky44 <120761876+MrLucky44@users.noreply.github.com>
2024-12-22 23:35:08 +0300

    front-end integration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
   get "contact" => "pages#contact"
-  get "index" => "pages#index"
+  get "index", to: "pages#index" 
   get "profile" => "pages#profile"
   # Authorization
   get "sign_up", to: "users#sign_up", as: :sign_up
