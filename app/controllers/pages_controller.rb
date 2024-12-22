98b2d83a9355a88090000be1792a31bad89446a2
g3yk <g3yk@users.noreply.github.com>
2024-12-22 15:17:11 +0300

    fix(index, profile): Fixed user authenticated check

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :profile]
+
   def home
   end
+
   def index
+    @user = current_user
   end
+
   def about
   end
+
   def profile
+    @user = current_user
   end
+
   def contact
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get about" do
