8daaf0d25315b233a59f0d1728c315a72c67a20c
g3yk <g3yk@users.noreply.github.com>
2024-12-25 22:22:57 +0300

    feat(notification): Add header notification templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Authentication
+  before_action :get_notification
+
+  def get_notification
+    @notifications = [
+      "Notification 1",
+      "Notification 2",
+      "Notification 3"
+    ]
+  end
 end
